eb81f3678d5e19a43c36f84d8eded31817af15b4
Anjali <anjalioraon6872@gmail.com>
2026-06-12 15:48:37 +0530

    Initial commit - scoreBoard website

---
+++ index.js
@@ -0,0 +1,36 @@
+let score1El = document.getElementById("score1-el")
+let score1 = 12
+
+function add1(){
+    score1 += 1
+    score1El.textContent = score1
+}
+
+function add2(){
+    score1 += 2
+    score1El.textContent = score1
+}
+
+function add3(){
+    score1 += 3
+    score1El.textContent = score1
+}
+
+let score2El = document.getElementById("score2-el")
+let score2 = 5
+
+function addd1(){
+    score2 += 1
+    score2El.textContent = score2
+}
+
+function addd2(){
+    score2 += 2
+    score2El.textContent = score2
+}
+
+function addd3(){
+    score2 += 3
+    score2El.textContent = score2
+}
+
